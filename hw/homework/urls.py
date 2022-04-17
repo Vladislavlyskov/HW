dd8e9687ad6f14f556b748d2a09fe5e52dc78bde
user <vladislavlyskov@gmail.com>
2022-04-17 21:05:46 +0300

    hw 13.04

--- hw/homework/urls.py
+++ hw/homework/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from homework.views import MainView
 
 urlpatterns = [
-    path('index/', MainView.as_view(), name="form_profile"),
+    path('name/', MainView.as_view(), name="form_profile"),
 ]
